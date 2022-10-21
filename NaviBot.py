963c13fa46ae3b857c032b375f979096a52c916e
Jimpie1043 <fx.leclerc05@gmail.com>
2022-10-20 22:16:37 -0400

    Improve play command

--- NaviBot.py
+++ NaviBot.py
@@ -11,27 +11,19 @@ from time import sleep
 from datetime import datetime
 from pytz import timezone
 from termcolor import colored
+import threading
 
 
     #Initializing the bot
 config = configparser.ConfigParser()
-config.read(r'C:\Users\super\OneDrive\Bureau\Python-Projects\Navi-Bot\config.txt')
+config.read(r'C:\Users\super\OneDrive\Bureau\Python\Navi-Bot\config.txt')
 token = config.get('CONFIG', 'DISCORD_TOKEN')
 intent = discord.Intents.all()
 intent.members = True
 intent.message_content = True
 default_prefix = '!'
 bot = commands.Bot(command_prefix=default_prefix, intents = intent)        #Sets the prefix and intents for all commands
-global download_mp3_limit
-download_mp3_limit = 1       #Limits the use of the download_mp3 command
-global download_mp3_uses
-download_mp3_uses = 0
-global play_limit
-play_limit = 1       #Limits the use of the play command
-global play_uses
-play_uses = 0
 listening = True        #If this is set to False, the bot won't listen to any command, except for !connect which turns this variable back to True and !exit which exits the code completely
-tz = timezone('America/New_York')        #Eastern Time timezone
 
 
     #Defining events
@@ -74,6 +66,7 @@ async def on_message(message):      #This section of code will create a log of a
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     channel = '#' + str(message.channel.name)
+    tz = timezone('America/New_York')        #Eastern Time timezone
     message_time = datetime.now(tz)     #Gets the current time with the specified timezone
     formatted_message_time = message_time.strftime("%b-%d-%Y %H:%M:%S")     #Changes the time to the specified format (Ex: Oct-18-2022 13:28:08)
     print(f'{colored(formatted_message_time, "grey")} {colored(channel.upper(), "blue", attrs=["bold"])}    {colored(username, "red", attrs=["bold"])}: {user_message}')
@@ -128,6 +121,13 @@ async def ping(ctx):        #Ping command
         await ctx.send(f"I am currently set to answer <@!309650289932369922>'s commands only. Please try again later.")
 
 
+#Download_mp3 variables
+global download_mp3_limit
+download_mp3_limit = 1       #Limits the !download_mp3 command to 1 use
+global download_mp3_uses
+download_mp3_uses = 0       #Represents the amount of times the !download_mp3 command is being used
+
+
 @bot.command(name='download_mp3', aliases = ['download.mp3', 'download_audio'], description='Download a video from youtube as a .mp3 file.')
 async def download_mp3(ctx, url):        #Download yt audio command
     if listening == True:       #Listening will be set to False if !disconnect is used
@@ -180,44 +180,98 @@ async def clear(ctx, amount=None):       #Clear command (default value is None)
         await ctx.send(f"I am currently set to answer <@!309650289932369922>'s commands only. Please try again later.")
 
 
-@bot.command(name='play', description='Plays music in the vc where users are currently connected or the first vc it finds.')
-async def play(ctx, url, vc=""):        #Play command
+#!play command variables
+global play_limit
+play_limit = 1       #Limits the !play command to n uses
+global play_uses
+play_uses = 0       #Represents the amount of times the !play command is being used
+global song_list
+song_list = []      #Creates a list of youtube url's. Every time you use the !play command, the url will get added to the list and be used for the next song
+global song_number
+song_number = 0
+
+
+async def after_play(ctx, file_name):       #After song stops playing in !play command
+    os.remove(rf'D:\Downloads\videos\{file_name}')
+    global song_list
+    if list != []:
+        await play(ctx, song_list[0])       #If the list is not empty, plays the next song in the list after the bot is done playing the current song and removes it from the list
+        song_list.remove[0]
+
+
+
+async def play_download_mp3(ctx, url, vc):      #Download and play the .mp3 file for the !play command
+    yt = pytube.YouTube(url)
+    filesize = yt.streams.first().filesize
+    if filesize < 24000000:       #Checks if the file is bigger than 24 MB
+        title = yt.streams[0].title
+        if filesize > 12000000:     #If we're dealing with a bigger file, display longer download time
+            await ctx.send(f'Downloading "{title}". This could take up to 30 seconds.')
+        elif filesize > 6000000:
+            await ctx.send(f'Downloading "{title}". This should take less than 20 seconds.')
+        stream = yt.streams.first()
+        global song_number
+        file_name = f'video{song_number}.mp3'       #What the file is going to be saved as in the computer
+        stream.download(output_path=r'D:\Downloads\videos', filename=file_name)
+        await ctx.send(f'Playing "{title}" in {vc}.')
+        print(vc)
+        connected = await vc.connect()
+        connected.play(discord.FFmpegPCMAudio(executable='ffmpeg.exe', source=rf'D:\Downloads\videos\{file_name}'), after=lambda e: (await after_play(ctx, file_name) for _ in '_').__anext__())
+                #Plays the song that's been downloaded and goes to after_play() after it's done
+        sleep(5)        #5 second delay to prevent anyone from spamming this command
+        return
+    else:
+        await ctx.send(f'Error: File is bigger than 24 MB. Max video length is about 40 minutes.')
+        sleep(3)
+        return
+
+
+@bot.command(name='play', description='Plays music in the vc where users are currently connected.')
+async def play(ctx, url):        #Play command
     if listening == True:       #Will be set to False if !disconnect is used
-        global play_uses
-        play_uses += 1
-        if play_uses <= play_limit:       #Represents the amount of times this command is being used at a time compared to the limit that is allowed (default is 1)
-            if ctx.author.voice and ctx.author.voice.channel and vc == "":
-                vc = ctx.author.voice.channel
-                yt = pytube.YouTube(url)
-                filesize = yt.streams.first().filesize
-                if filesize < 24000000:       #Checks if the file is bigger than 24 MB
-                    title = yt.streams[0].title
-                    if filesize > 12000000:     #If we're dealing with a bigger file, display longer download time
-                        await ctx.send(f'Downloading "{title}". This could take up to 30 seconds.')
-                    elif filesize > 6000000:
-                        await ctx.send(f'Downloading "{title}". This should take less than 20 seconds.')
-                    stream = yt.streams.first()
-                    stream.download(output_path=r'D:\Downloads\videos', filename=f'{title}.mp3')
-                    await ctx.send(f'Playing "{title}" in {vc}.')
-                    connected = await vc.connect()
-                    connected.play(discord.FFmpegPCMAudio(executable='ffmpeg.exe', source=rf'D:\Downloads\videos\{title}.mp3'), after=lambda e: os.remove(rf'D:\Downloads\videos\{title}.mp3'))
-                    sleep(5)        #5 second delay to prevent anyone from spamming this command
+        voice = discord.utils.get(bot.voice_clients, guild=ctx.guild) # This allows for more functionality with voice channels
+        if voice != None:       #If bot is currently playing something, add url to the list
+            global song_list
+            song_list += url
+        else:
+            global play_uses
+            play_uses += 1
+            if play_uses <= play_limit:       #Represents the amount of times this command is being used at a time compared to the limit that is allowed (default is 1)
+                if ctx.author.voice and ctx.author.voice.channel:
                     play_uses -= 1
+                    vc = ctx.author.voice.channel
+                    await play_download_mp3(ctx, url, vc)
+                    return
                 else:
-                    await ctx.send(f'Error: File is bigger than 24 MB. Max video length is about 40 minutes.')
-                    sleep(3)
+                    await ctx.send(f'You need to be in a vc or to specify the name of the vc to use this command!')
                     play_uses -= 1
             else:
-                await ctx.send(f'You need to be in a vc or to specify the name of the vc to use this command!')
+                await ctx.send(f'Please do not spam this command!')
+                sleep(3)
                 play_uses -= 1
-                return
-        else:
-            await ctx.send(f'Please do not spam this command!')
-            sleep(3)
-            play_uses -= 1
     else:
         await ctx.send(f"I am currently set to answer <@!309650289932369922>'s commands only. Please try again later.")
+        play_uses -= 1
+
+
+@bot.command(name='playlist', description='Shows you a list of the songs that are about to play.')
+async def playlist(ctx):        #List command
+    if listening == True:
+        global song_list
+        await ctx.send(f"Here's the playlist: {song_list}")
+    else:
+        await ctx.send(f"I am currently set to answer <@!309650289932369922>'s commands only. Please try again later.")
+        play_uses -= 1
 
 
     #Running the bot
+"""def printEveryNSeconds():       #Prints the value of something every 5 seconds if you need to debug
+  threading.Timer(5.0, printit).start()
+  global play_uses
+  print(play_uses)
+
+printEveryNSeconds()"""
+
+
+
 bot.run(token)
